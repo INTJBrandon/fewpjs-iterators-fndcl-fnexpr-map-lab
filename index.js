bcec315d07b903eae66c294f8ccb2642ea195534
seireitou <ausis@mail.com>
2021-04-28 23:43:42 -0500

    commit tree a639108c7cbe10770d2e552c3c01b8cdb2071ea3

--- index.js
+++ index.js
@@ -12,5 +12,9 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(lessonName => {
+    const words = lessonName.split(" ")
+    const capitalizedWords = words.map(word => word[0].toUpperCase() + word.slice(1))
+    return capitalizedWords.join(" ")
+  })
 }
